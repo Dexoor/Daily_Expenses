ed6db15ca3784d9817808a0a5ff342b35cf1d287
Dexoor <xdexor@gmail.com>
2021-05-06 19:13:10 +0200

    Update app.py
    Qt designer UI added

--- app.py
+++ app.py
@@ -1,115 +1,253 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+row1 = [0]
+row2 = [0]
 
 
-def UpdateResult1():
-    text = str(amount1.text())
-    result1.append(text + " PLN")
-    row1.append(int(text))
-    amount1.clear()
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(415, 499)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        sizePolicy = QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridFrame = QtWidgets.QFrame(self.centralwidget)
+        self.gridFrame.setGeometry(QtCore.QRect(11, 1, 391, 321))
+        self.gridFrame.setObjectName("gridFrame")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridFrame)
+        self.gridLayout.setObjectName("gridLayout")
+        self.name2 = QtWidgets.QLabel(self.gridFrame)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        font.setBold(True)
+        font.setWeight(75)
+        self.name2.setFont(font)
+        self.name2.setAlignment(QtCore.Qt.AlignCenter)
+        self.name2.setObjectName("name2")
+        self.gridLayout.addWidget(self.name2, 0, 5, 1, 1)
+        self.name1 = QtWidgets.QLabel(self.gridFrame)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        font.setBold(True)
+        font.setWeight(75)
+        self.name1.setFont(font)
+        self.name1.setAlignment(QtCore.Qt.AlignCenter)
+        self.name1.setObjectName("name1")
+        self.gridLayout.addWidget(self.name1, 0, 2, 1, 1)
+        self.col2 = QtWidgets.QTextBrowser(self.gridFrame)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(11)
+        self.col2.setFont(font)
+        self.col2.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.col2.setStyleSheet("")
+        self.col2.setObjectName("col2")
+        self.gridLayout.addWidget(self.col2, 1, 5, 1, 1)
+        self.col1 = QtWidgets.QTextBrowser(self.gridFrame)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(11)
+        self.col1.setFont(font)
+        self.col1.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.col1.setObjectName("col1")
+        self.gridLayout.addWidget(self.col1, 1, 2, 1, 1)
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(11, 331, 391, 163))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.lcdNumber1 = QtWidgets.QLCDNumber(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(11)
+        self.lcdNumber1.setFont(font)
+        self.lcdNumber1.setStyleSheet("background-color: rgb(75, 142, 200);")
+        self.lcdNumber1.setSmallDecimalPoint(False)
+        self.lcdNumber1.setSegmentStyle(QtWidgets.QLCDNumber.Flat)
+        self.lcdNumber1.setObjectName("lcdNumber1")
+        self.gridLayout_2.addWidget(self.lcdNumber1, 3, 0, 1, 1)
+        self.Value1 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.Value1.setFont(font)
+        self.Value1.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.Value1.setText("")
+        self.Value1.setAlignment(
+            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.Value1.setObjectName("Value1")
+        self.Value1.returnPressed.connect(UpdateResult1)
+        self.gridLayout_2.addWidget(self.Value1, 1, 0, 1, 1)
+        self.ClearButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        self.ClearButton.pressed.connect(ClearButton)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(9)
+        self.ClearButton.setFont(font)
+        self.ClearButton.setObjectName("ClearButton")
+        self.gridLayout_2.addWidget(self.ClearButton, 6, 1, 1, 1)
+        self.LoadButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(9)
+        self.LoadButton.setFont(font)
+        self.LoadButton.setStyleSheet("\n"
+                                      "selection-background-color: rgb(250, 216, 255);")
+        self.LoadButton.setObjectName("LoadButton")
+        self.gridLayout_2.addWidget(self.LoadButton, 5, 1, 1, 1)
+        self.comboBox = QtWidgets.QComboBox(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(11)
+        self.comboBox.setFont(font)
+        self.comboBox.setObjectName("comboBox")
+        self.gridLayout_2.addWidget(self.comboBox, 5, 0, 2, 1)
+        self.Value2 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.Value2.setFont(font)
+        self.Value2.setText("")
+        self.Value2.setAlignment(
+            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.Value2.setClearButtonEnabled(False)
+        self.Value2.setObjectName("Value2")
+        self.Value2.returnPressed.connect(UpdateResult2)
+        self.gridLayout_2.addWidget(self.Value2, 1, 1, 1, 1)
+        self.lcdNumber2 = QtWidgets.QLCDNumber(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(11)
+        self.lcdNumber2.setFont(font)
+        self.lcdNumber2.setStyleSheet("background-color: rgb(75, 142, 200);")
+        self.lcdNumber2.setSegmentStyle(QtWidgets.QLCDNumber.Flat)
+        self.lcdNumber2.setObjectName("lcdNumber2")
+        self.gridLayout_2.addWidget(self.lcdNumber2, 3, 1, 1, 1)
+        self.Difference = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Comic Sans MS")
+        font.setPointSize(12)
+        self.Difference.setFont(font)
+        self.Difference.setText("")
+        self.Difference.setAlignment(QtCore.Qt.AlignCenter)
+        self.Difference.setReadOnly(True)
+        self.Difference.setObjectName("Difference")
+        self.gridLayout_2.addWidget(self.Difference, 4, 0, 1, 2)
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 421, 505))
+        self.frame.setStyleSheet("background-color: rgb(152, 144, 154);\n"
+                                 "background-color: qlineargradient(spread:repeat, x1:1, y1:1, x2:1, y2:0, stop:0 rgba(138, 106, 194, 255), stop:1 rgba(255, 255, 255, 255))\n"
+                                 "\n"
+                                 "")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.frame.raise_()
+        self.gridFrame.raise_()
+        self.gridLayoutWidget.raise_()
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate(
+            "MainWindow", "Wydatki mieszkaniowe"))
+        self.name2.setText(_translate("MainWindow", "Wydatki Daniel"))
+        self.name1.setText(_translate("MainWindow", "Wydatki Ola"))
+        self.col2.setToolTip(_translate(
+            "MainWindow", "<html><head/><body><p align=\"right\"><br/></p></body></html>"))
+        self.col2.setWhatsThis(_translate(
+            "MainWindow", "<html><head/><body><p align=\"right\"><br/></p></body></html>"))
+        self.col2.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                     "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                     "p, li { white-space: pre-wrap; }\n"
+                                     "</style></head><body style=\" font-family:\'Comic Sans MS\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+                                     "<p align=\"right\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.col1.setWhatsThis(_translate(
+            "MainWindow", "<html><head/><body><p align=\"right\"><br/></p></body></html>"))
+        self.col1.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                     "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                     "p, li { white-space: pre-wrap; }\n"
+                                     "</style></head><body style=\" font-family:\'Comic Sans MS\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+                                     "<p align=\"right\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.Value1.setToolTip(_translate(
+            "MainWindow", "<html><head/><body><p align=\"right\"><br/></p></body></html>"))
+        self.Value1.setWhatsThis(_translate(
+            "MainWindow", "<html><head/><body><p align=\"right\"><br/></p></body></html>"))
+        self.ClearButton.setText(_translate("MainWindow", "Clear"))
+        self.LoadButton.setText(_translate("MainWindow", "Load"))
+        self.comboBox.setToolTip(_translate(
+            "MainWindow", "<html><head/><body><p align=\"center\"><br/></p></body></html>"))
+        self.comboBox.setWhatsThis(_translate(
+            "MainWindow", "<html><head/><body><p align=\"center\"><br/></p></body></html>"))
+        self.Value2.setToolTip(_translate(
+            "MainWindow", "<html><head/><body><p align=\"right\"><br/></p></body></html>"))
+        self.Value2.setWhatsThis(_translate(
+            "MainWindow", "<html><head/><body><p align=\"right\"><br/></p></body></html>"))
+
+
+def UpdateResult1():  # po wpisaniu kwoty i wcisnieciu enter kolumna 1
+    text1 = str(ui.Value1.text())
+    ui.col1.append(text1 + " PLN")
+    row1.append(int(text1))
+    ui.Value1.clear()
     sum1 = sum(row1, 0)
-    add_up1.setText("SUMA               " + str(sum1))
+    ui.lcdNumber1.display(sum1)
     DifferenceUpdate()
+    # SaveDataTxt1()
 
 
-def UpdateResult2():
-    text1 = str(amount2.text())
-    result2.append(text1 + " PLN")
-    row2.append(int(text1))
-    amount2.clear()
+def UpdateResult2():  # po wpisaniu kwoty i wcisnieciu enter kolumna 2
+    text2 = str(ui.Value2.text())
+    ui.col2.append(text2 + " PLN")
+    row2.append(int(text2))
+    ui.Value2.clear()
     sum2 = sum(row2, 0)
-    add_up2.setText("SUMA               " + str(sum2))
+    ui.lcdNumber2.display(sum2)
     DifferenceUpdate()
+    # SaveDataTxt1()
 
 
-def DifferenceUpdate():
+def DifferenceUpdate():  # roznica miedzy kolumnami
     substraction = ((sum(row1, 0)) - (sum(row2, 0)))
     if substraction == 0:
-        Difference.setText("Jesteśmy kwita")
+        ui.Difference.setText("Jesteśmy kwita")
     elif substraction > 0:
-        Difference.setText("Daniel jest winny:  " + str(substraction))
+        ui.Difference.setText("Daniel jest winny:  " + str(substraction))
     elif substraction < 0:
-        Difference.setText("Ola jest winna:  " + str(-substraction))
+        ui.Difference.setText("Ola jest winna:  " + str(-substraction))
 
 
-# Empty Lists
-row2 = [0]
-row1 = [0]
+def ClearButton():
+    ui.col1.clear()
+    ui.col1.setAlignment(QtCore.Qt.AlignRight)
+    ui.col2.clear()
+    ui.col2.setAlignment(QtCore.Qt.AlignRight)
+    row1.clear()
+    row2.clear()
+    ui.Difference.clear()
+    ui.lcdNumber1.display(0)
+    ui.lcdNumber2.display(0)
 
 
-# Main Window
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle("Wydatki mieszkaniowe")
-window.setFixedWidth(400)
-window.setFixedHeight(250)
-
-
-# Widgets
-grid = QGridLayout()
-result1 = QTextBrowser()
-result1.setAlignment(Qt.AlignRight)
-result1.setFont(QFont("Arial", 12))
-
-result2 = QTextBrowser()
-result2.setAlignment(Qt.AlignRight)
-result2.setFont(QFont("Arial", 12))
-
-amount1 = QLineEdit()
-amount1.setValidator(QIntValidator())
-amount1.setFont(QFont("Arial", 12))
-amount1.setAlignment(Qt.AlignRight)
-
-amount2 = QLineEdit()
-amount2.setValidator(QIntValidator())
-amount2.setFont(QFont("Arial", 12))
-amount2.setAlignment(Qt.AlignRight)
-
-description_row1 = QLabel("Wydatki Ola")
-description_row1.setFont(QFont("Arial", 16))
-description_row1.setAlignment(Qt.AlignCenter)
-
-description_row2 = QLabel("Wydatki Daniel")
-description_row2.setFont(QFont("Arial", 16))
-description_row2.setAlignment(Qt.AlignCenter)
-
-add_up1 = QLineEdit("SUMA                   ")
-add_up1.setAlignment(Qt.AlignRight)
-add_up1.setFont(QFont("Arial", 12))
-add_up1.setReadOnly(True)
-
-add_up2 = QLineEdit("SUMA                  ")
-add_up2.setAlignment(Qt.AlignRight)
-add_up2.setFont(QFont("Arial", 12))
-add_up2.setReadOnly(True)
-
-Difference = QLineEdit()
-Difference.setReadOnly(True)
-Difference.setFont(QFont("Arial", 12))
-Difference.setAlignment(Qt.AlignCenter)
-
-months = QComboBox()
-
-# Layout
-grid.addWidget(description_row1, 0, 0)
-grid.addWidget(description_row2, 0, 1)
-grid.addWidget(result1, 1, 0)
-grid.addWidget(result2, 1, 1)
-grid.addWidget(amount1, 2, 0)
-grid.addWidget(amount2, 2, 1)
-grid.addWidget(add_up1, 3, 0)
-grid.addWidget(add_up2, 3, 1)
-grid.addWidget(Difference, 4, 0, 1, 2)
-
-grid.addWidget(months, 5, 0)
-window.setLayout(grid)
-
-# Value entering
-amount1.returnPressed.connect(UpdateResult1)
-amount2.returnPressed.connect(UpdateResult2)
-
-
-window.show()
-sys.exit(app.exec())
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
